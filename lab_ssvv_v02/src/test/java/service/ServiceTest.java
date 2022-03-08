6b76abe05c242dd2643ed7988c88d2e0eb7b16a9
Ioana Serban <ioana_serban_11@yahoo.com>
2022-03-08 20:07:43 +0200

    class added ServiceTest

---
+++ lab_ssvv_v02/src/test/java/service/ServiceTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    void addStudent_validInexistentID_Created() {
+        //returns null
+
+    }
+
+    @Test
+    void addStudent_validExistentID_NotCreated() {
+        //returns entity that we wanted to add
+
+    }
+}
