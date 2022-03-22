49789eb6bf4b9c1e68c3210ac41e4c82ec92ce42
Ioana Serban <ioana_serban_11@yahoo.com>
2022-03-22 16:38:36 +0200

    added all missing testcases

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -32,38 +32,53 @@ public class ServiceTest {
     }
 
 
+
+    //testcase 3
     @Test
     @Order(1)
     public void addStudent_validInexistentID_Created() {
-        service.addStudent(new Student("1", "vlad", 936, "rvie2810@scs.ubbcluj.ro"));
+        Student student = service.addStudent(new Student("112", "ioana", 936, "siie2810@scs.ubbcluj.ro"));
 
+        assertNull(student);
         assertEquals(1, Stream.of(service.getAllStudenti().spliterator()).count());
     }
 
+    //testcase 4
     @Test
     @Order(2)
     public void addStudent_validExistentID_NotCreated() {
         //returns entity that we wanted to add
-        service.addStudent(new Student("1", "vlad new", 936, "someemail@scs.ubbcluj.ro"));
+        Student student = service.addStudent(new Student("112", "maria", 936, "someemail@scs.ubbcluj.ro"));
 
+        assertEquals("ioana", student.getNume());
         assertEquals(1, Stream.of(service.getAllStudenti().spliterator()).count());
-
-        assertEquals("vlad", service.getAllStudenti().iterator().next().getNume());
     }
 
+    //testcase1
     @Test
-    public void addStudent_invalidInexistentID_NotCreated(){
+    public void addStudent_invalidEmptyID_NotCreated(){
         try{
             service.addStudent(new Student("", "someone", 936, "sta"));
             fail("validation exception not thrown");
         }catch (ValidationException exception){
 
         }
-
     }
 
+    //testcase2
     @Test
-    public void addStudent_invalidName_NotCreated(){
+    public void addStudent_invalidNullID_NotCreated(){
+        try{
+            service.addStudent(new Student(null, "someone", 936, "sta"));
+            fail("validation exception not thrown");
+        }catch (ValidationException exception){
+
+        }
+    }
+
+    //testcase5
+    @Test
+    public void addStudent_invalidEmptyName_NotCreated(){
         try {
             service.addStudent(new Student("some_id", "", 936, "sta"));
             fail("validation exception not thrown");
@@ -72,23 +87,72 @@ public class ServiceTest {
         }
     }
 
+    //testcase6
     @Test
-    public void addStudent_invalidGroup_NotCreated(){
+    public void addStudent_invalidNullName_NotCreated(){
         try {
-            service.addStudent(new Student("some_id", "some name", -200, "sta"));
+            service.addStudent(new Student("some_id", null, 936, "sta"));
             fail("validation exception not thrown");
         }catch (ValidationException exception){
 
         }
     }
 
+    //testcase7
     @Test
-    public void addStudent_invalidEmail_NotCreated(){
+    public void addStudent_validName_Created(){
+        //nume "ana"
+        //verificare ca la testcase 3 & 4
+    }
+
+    //testcase8
+    @Test
+    public void addStudent_validPositiveGroup_Created(){
+        //group 0
+        //group 1
+        //verificare ca la testcase 3 & 4
+
+    }
+
+    //testcase9
+    @Test
+    public void addStudent_invalidNegativeGroup_NotCreated(){
+        try {
+            service.addStudent(new Student("some_id", "some name", -1, "sta"));
+            fail("validation exception not thrown");
+        }catch (ValidationException exception){
+
+        }
+    }
+
+
+    //testcase 12
+    @Test
+    public void addStudent_invalidEmptyEmail_NotCreated(){
         try {
             service.addStudent(new Student("some_id", "some name", 936, ""));
             fail("validation exception not thrown");
         }catch (ValidationException exception){
 
         }
+    }
+
+    //testcase 13
+    @Test
+    public void addStudent_invalidNullEmail_NotCreated(){
+        try {
+            service.addStudent(new Student("some_id", "some name", 936, ""));
+            fail("validation exception not thrown");
+        }catch (ValidationException exception){
+
+        }
+    }
+
+    //testcase 14
+    @Test
+    public void addStudent_validEmail_Created(){
+        //email "ana"
+        //verificare ca la testcase 3 & 4
+
     }
 }
